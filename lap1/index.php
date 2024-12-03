3115885c8b6f0fb754e5b3f86b76aaa42f7950e3
ماريا العدوف <mariasadiqmohmmedsaif@gmail.com>
2024-12-03 11:08:02 -0800

    Add files via upload

---
+++ lap1/index.php
@@ -0,0 +1,21 @@
+<?php
+//$x = "the is a string variable";
+//$y = "22" ;
+
+//$x = "the is a integer variable";
+
+$x = 100;
+$y = 22;
+$z =$x + $y;
+echo $z;
+var_dump($z);
+
+
+
+
+
+
+
+
+;?>
+
---
+++ lap1/var.php
@@ -0,0 +1,29 @@
+<?php
+//$x = "the is a string variable";
+//$y = "maria_22" ;
+
+//$v = "the is a integer variable";
+
+// $x = 100;
+// $y = 22;
+// $z =$x + $y;
+// echo $z;
+// var_dump($z);
+
+//$v = "the is a float/double variable";
+
+// $m = 100.5;
+// var_dump($m);
+
+
+//$v = "the is a BOOLEAN variable";
+// $m =true;
+// $n = false;
+// var_dump($m);
+
+//$x = "the is a Array variable";
+// $x = array(1,2,3,4);
+// var_dump($x);
+
+;?>
+
